0bb2622b6e9a409fab8289071138980468150b9d
Abhijit Kulkarni <ak779531121@gmail.com>
2019-01-27 15:24:43 +0530

    Algo Semi Done

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # Importing Modules..
 from tkinter import *
 from time import sleep
+import random
+
+random.seed()
 
 global POS
-global TRUN
+global TURN
 TURN = 0
-
+DIFF = []
 POS_LEFT = [1,2,3,4,5,6,7,8,9]
-POS_SEL = []
+COM_POS_SEL = []
+COM_POS = [1,3,7,9]
+TEMP = {}
+b = True
+
 def myturn(event):
     # print("clicked at ", event.x, event.y)
     if event.x >= 120 and event.x < 240 and event.y >= 40 and event.y < 140:
@@ -43,10 +50,29 @@ def myturn(event):
     if POS!=-1:
         TURN = 0
         POS_LEFT.remove(POS)
-        POS_SEL.append(POS)
+        if POS in COM_POS:
+            COM_POS.remove(POS)
     print(POS)
 
+def Change():
+    TURN = 0
 
+
+def comturn():
+    if len(COM_POS) > 0:
+        POS = random.choice(COM_POS)    
+        if POS == 1:
+            my_canvas.create_text(180, 90, font = ("Callibri",20), text = 'O')
+        elif POS == 3:
+            my_canvas.create_text(420, 90, font = ("Callibri",20), text = 'O')
+        elif POS == 7:
+            my_canvas.create_text(180, 310, font = ("Callibri",20), text = 'O')
+        else:
+            my_canvas.create_text(420, 310, font = ("Callibri",20), text = 'O')
+    else:
+        print("EMPTY")
+    TURN = 1
+    COM_POS_SEL.append(POS)
 
 
 # Main()
@@ -55,9 +81,6 @@ root = Tk()
 my_canvas = Canvas(root, width = 640, height = 480, background = 'white')
 my_canvas.pack()
 root.bind("<Button-1>", myturn)
-if TURN == 0:
-    sleep(3)
-    TURN = 1
 # Drawing Lines...
 # Vertical Lines.....
 my_canvas.create_line(240,40,240,360)
@@ -82,10 +105,34 @@ my_canvas.create_text(610,15, text = 'YOU - X')
 my_canvas.create_text(610,30, text = 'COM - O')
 my_canvas.create_text(300,15, text = 'CLICK TO PLACE')
 
-
-if(TURN == 1):
-    my_canvas.create_text(40, 15, text = 'YOUR TURN')
-else:
-    my_canvas.create_text(40, 15, text = "MY TURN.")
-
+while True:
+    if(TURN == 1):
+        my_canvas.create_text(40, 15, text = 'YOUR TURN')
+        print(TURN)
+    else:
+        my_canvas.create_text(40, 15, text = "MY TURN.")
+        if(len(COM_POS_SEL)>=3):
+            i = 0
+            while i<len(COM_POS_SEL)-1:
+                j = i+1
+                while j<len(COM_POS_SEL):
+                    DIFF.append(abs(COM_POS_SEL[j] - COM_POS_SEL[i]))
+                    j = j + 1
+                i = i + 1
+            for x in DIFF:
+                TEMP[x] = DIFF.count(x)
+            for x in TEMP:
+                if(TEMP[x] >=3 ):
+                    b = False
+        if(not b):
+            print('GameOver! Computer WON the Game.')
+            # root.destroy()
+            break
+        else:
+            # sleep(1)
+            comturn()
+    root.update()
+            
+        
+    
 root.mainloop()
